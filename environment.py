516dcb61ff4267d0fc402bb5cdf010ccf7d1c7c6
OSA7JIMI <126651855+OSA7JIMI@users.noreply.github.com>
2026-02-02 11:04:08 +0800

    Add files via upload

--- environment.py
+++ environment.py
@@ -7,7 +7,7 @@ class BuyKnowledge(gym.Env):
 
     metadata = {"render_modes": ["human"], "render_fps": 30}
     
-    def __init__(self, data = pd.DataFrame(), train_mode = True, sampling = True):
+    def __init__(self, data = pd.DataFrame(), train_mode = True):
         super().__init__()
         self.action_space = spaces.Box(low = 0.0, high = 1.0, shape = (2,), dtype = np.float64)
         self.observation_space = spaces.Box(low = -1.0, high = 100.0, shape = (27,), dtype = np.float64)
@@ -16,44 +16,31 @@ class BuyKnowledge(gym.Env):
         
         if self.train_mode:
             data = pd.read_csv('buy_norm.csv')
-
+            
             above = data[data['signal_return'] > 10]
             below = data[data['signal_return'] <= 10]
             frac = len(above)/len(below)
-            
-            if sampling: # buy signals with a return of 10 % or more are used in the same proportion as those that do not achieve this return [Jeong & Gu]
+            if frac >1:
+                frac = 1/frac
                 data = pd.concat([above.sample(frac = frac), below]).sort_index().reset_index(drop=True)
-            else: 
-                penalty_big = (1-2*frac)/(frac-1) * 0.85
-                reward_big = int((1-frac)*(1+penalty_big)/frac) + 1
-                self.penalty_big = penalty_big
-                self.reward_big = reward_big
-                print(f'Penalty: {penalty_big}')
-                print(f'Reward: {reward_big}')
-                print(f'Maximum reward: {len(below) + reward_big*len(above)}')
+            else:
+                data = pd.concat([above, below.sample(frac = frac)]).sort_index().reset_index(drop=True)
                 
             data = data.sample(frac=1) 
             self.returns = data['signal_return'] > 10
             data = data.iloc[:,:-3]
-                    
+        
         self.data = data
-        self.sampling = sampling
         self.num_steps = len(data)
         print(f'# of steps: {self.num_steps}')
         
     def step(self, action):
         if self.train_mode:
             if action[0] > action[1]:
-                if self.sampling:
-                    if self.returns.iloc[self.cur_step]:
-                        reward = 1
-                    else:
-                        reward = 0
+                if self.returns.iloc[self.cur_step]:
+                    reward = 1
                 else:
-                    if self.returns.iloc[self.cur_step]:
-                        reward = self.reward_big
-                    else:
-                        reward = self.penalty_big
+                    reward = 0
             else:
                 if self.returns.iloc[self.cur_step]:
                     reward = 0
@@ -73,7 +60,7 @@ class BuyKnowledge(gym.Env):
         pass
         
     def close(self):
-        pass      
+        pass
 
 class SellKnowledge(gym.Env):
 
